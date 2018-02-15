39cef8c26cac06ebc6f00de094767c8200bd56c8
Mary <rtutmash@gmail.com>
2018-02-15 17:54:56 +0300

    Add full

---
+++ Assets/Scripts/loading.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class loading : MonoBehaviour {
+
+    IEnumerator Start()
+    {
+        AsyncOperation async = Application.LoadLevelAsync("workspace");
+        yield return async;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
